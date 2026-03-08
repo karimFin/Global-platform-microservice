54d2ba24a9316df6d7a81fbd36724ab349ebf5b8
karimfin <karimmdmirajul@gmail.com>
2026-03-09 00:07:47 +0200

    Add Terraform secret governance policy and rotation workflow scaffold

--- infra/terraform/envs/github/main.tf
+++ infra/terraform/envs/github/main.tf
@@ -2,6 +2,42 @@ data "github_repository" "this" {
   full_name = "${var.github_owner}/${var.repository_name}"
 }
 
+locals {
+  required_secret_environments = toset(keys(var.required_environment_secrets))
+  declared_secret_environments = toset(keys(var.declared_environment_secret_names))
+  managed_secret_environments  = toset(keys(var.managed_environment_secrets))
+  managed_environment_secret_entries = flatten([
+    for env_name, secret_map in var.managed_environment_secrets : [
+      for secret_name, secret_value in secret_map : {
+        key   = "${env_name}:${secret_name}"
+        env   = env_name
+        name  = secret_name
+        value = secret_value
+      }
+    ]
+  ])
+}
+
+check "required_secrets_are_declared" {
+  assert {
+    condition = alltrue([
+      for env_name, required_names in var.required_environment_secrets :
+      length(setsubtract(toset(required_names), toset(lookup(var.declared_environment_secret_names, env_name, [])))) == 0
+    ])
+    error_message = "required_environment_secrets must be included in declared_environment_secret_names for each environment."
+  }
+}
+
+check "managed_secrets_are_declared" {
+  assert {
+    condition = alltrue([
+      for env_name, secret_map in var.managed_environment_secrets :
+      length(setsubtract(toset(keys(secret_map)), toset(lookup(var.declared_environment_secret_names, env_name, [])))) == 0
+    ])
+    error_message = "managed_environment_secrets contains secret names not listed in declared_environment_secret_names."
+  }
+}
+
 resource "github_issue_label" "preview" {
   repository  = var.repository_name
   name        = "preview"
@@ -84,6 +120,17 @@ resource "github_repository_environment" "prod" {
   }
 }
 
+resource "github_repository_environment" "secret_policy_environments" {
+  for_each    = setsubtract(setunion(local.required_secret_environments, local.declared_secret_environments, local.managed_secret_environments), toset(["dev", "prod"]))
+  repository  = var.repository_name
+  environment = each.value
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = false
+  }
+}
+
 resource "github_actions_environment_variable" "dev_tf_org" {
   count = var.tf_cloud_organization == "" ? 0 : 1
 
@@ -98,4 +145,51 @@ resource "github_actions_environment_variable" "dev_tf_workspace" {
   environment   = github_repository_environment.dev.environment
   variable_name = "TF_WORKSPACE"
   value         = var.tf_workspace
+}
+
+resource "github_actions_environment_variable" "secret_policy_required" {
+  for_each = var.required_environment_secrets
+
+  repository    = var.repository_name
+  environment   = each.key
+  variable_name = "REQUIRED_SECRETS"
+  value         = join(",", each.value)
+
+  depends_on = [
+    github_repository_environment.dev,
+    github_repository_environment.prod,
+    github_repository_environment.secret_policy_environments
+  ]
+}
+
+resource "github_actions_environment_variable" "secret_policy_declared" {
+  for_each = var.declared_environment_secret_names
+
+  repository    = var.repository_name
+  environment   = each.key
+  variable_name = "DECLARED_SECRETS"
+  value         = join(",", each.value)
+
+  depends_on = [
+    github_repository_environment.dev,
+    github_repository_environment.prod,
+    github_repository_environment.secret_policy_environments
+  ]
+}
+
+resource "github_actions_environment_secret" "managed" {
+  for_each = {
+    for entry in local.managed_environment_secret_entries : entry.key => entry
+  }
+
+  repository      = var.repository_name
+  environment     = each.value.env
+  secret_name     = each.value.name
+  plaintext_value = each.value.value
+
+  depends_on = [
+    github_repository_environment.dev,
+    github_repository_environment.prod,
+    github_repository_environment.secret_policy_environments
+  ]
 }
--- infra/terraform/envs/github/variables.tf
+++ infra/terraform/envs/github/variables.tf
@@ -42,3 +42,47 @@ variable "tf_workspace" {
   type    = string
   default = "gmp-dev"
 }
+
+variable "required_environment_secrets" {
+  type = map(list(string))
+  default = {
+    dev = [
+      "KUBE_CONFIG_DEV",
+      "TF_API_TOKEN"
+    ]
+    prod = [
+      "KUBE_CONFIG_PROD"
+    ]
+  }
+}
+
+variable "declared_environment_secret_names" {
+  type = map(list(string))
+  default = {
+    dev = [
+      "KUBE_CONFIG_DEV",
+      "TF_API_TOKEN",
+      "OCI_CLI_USER",
+      "OCI_CLI_TENANCY",
+      "OCI_CLI_FINGERPRINT",
+      "OCI_CLI_KEY_CONTENT",
+      "OCI_CLI_REGION",
+      "GHCR_TOKEN"
+    ]
+    prod = [
+      "KUBE_CONFIG_PROD",
+      "OCI_CLI_USER",
+      "OCI_CLI_TENANCY",
+      "OCI_CLI_FINGERPRINT",
+      "OCI_CLI_KEY_CONTENT",
+      "OCI_CLI_REGION",
+      "GHCR_TOKEN"
+    ]
+  }
+}
+
+variable "managed_environment_secrets" {
+  type      = map(map(string))
+  default   = {}
+  sensitive = true
+}
